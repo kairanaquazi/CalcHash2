7e7d6a3910517679235cf7beb95ab9149ed2b3dc
breisfeld <brad.reisfeld@colostate.edu>
2019-09-11 13:00:31 -0600

    Update internal messages and functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CalcHash
 
-A plugin for fman than computes the hash of the selected files and copies the results to the clipboard
+A plugin for `fman` that computes the hash of the selected files and copies the results to the clipboard.
+
+In slightly more detail: Select a file or files (not a directory or directories), press the assigned key (default is `F8`), and choose the hashing algorithm. Results will be copied to the clipboard.
--- calc_hash/__init__.py
+++ calc_hash/__init__.py
@@ -5,42 +5,52 @@ from fman.fs import is_dir
 from fman.url import as_human_readable
 
 hash_map = {
-    method: getattr(hashlib, method) for method in hashlib.hashlib.algorithms_guaranteed
+    method: getattr(hashlib, method) for method in hashlib.algorithms_guaranteed
 }
 
 
 class CalcHash(DirectoryPaneCommand):
     def __call__(self):
-        valid_methods = ", ".join(hash_map.keys())
+        valid_methods = "\n".join(f" - {hm}" for hm in hash_map.keys())
         chosen_files = self.get_chosen_files()
         results = []
-        if chosen_files:
+
+        # perform some checks on the selected files
+        # as of now, only individual files are allowed, not directories
+        dirs_in_selection = any([is_dir(fl) for fl in chosen_files])
+        if dirs_in_selection:
+            show_alert("Please specify individual files, not directories.")
+            return
+        else:
             hmeth, okay = show_prompt(
-                f"Enter one of the following hashing methods: {valid_methods}"
+                f"Enter one of the following hashing methods:\n{valid_methods}"
             )
             hmeth = hmeth.strip()
             if hmeth not in hash_map.keys():
-                response = show_alert("Invalid hash method.")
+                response = show_alert("Invalid hashing method. No action taken.")
                 return
-            if okay:
-                for fl in chosen_files:
-                    if not is_dir(fl):
-                        fhash = file_hash(fl, hmeth)
-                        hstr = f"{as_human_readable(fl)}: {fhash}"
-                        results.append(hstr)
-                print(results)
-                all_results = "\n".join(results)
-                clipboard.set_text(all_results)
-                response = show_alert(
-                    "File hash(es) have been copied to your clipboard."
-                )
-            else:
-                show_alert("No action taken.")
+
+        # perform the actual hash calculations
+        num_selected_files = len(chosen_files)
+        for fl in chosen_files:
+            fl = as_human_readable(fl)
+            fhash = file_hash(fl, hmeth)
+            hstr = f"{fl}: {fhash}"
+            results.append(hstr)
+            all_results = "\n".join(results)
+            clipboard.set_text(all_results)
+        if num_selected_files == 1:
+            msg = (
+                f"{hmeth} hash of the specified file has been copied to your clipboard."
+            )
+        else:
+            msg = f"{hmeth} hashes of the {num_selected_files} selected files have been copied to your clipboard."
+        response = show_alert(msg)
 
 
 def file_hash(filename, method="sha1"):
-    hashfunc = hash_map[method]
+    hashfunc = hash_map[method]()
     with open(filename, "rb") as f:
         for chunk in iter(lambda: f.read(8192), b""):
             hashfunc.update(chunk)
-    return hashfunc.digest()
+    return hashfunc.hexdigest()
